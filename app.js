2ba630384ebb3c96d56fb7189d65861371882106
ennypian adekoya <ennypitan@gmail.com>
2023-09-12 19:28:11 +0100

    updated

---
+++ app.js
@@ -0,0 +1,24 @@
+//Responsive menu
+
+const menuIcon = document.querySelector('.hamburger')
+const menuList = document.querySelector('.mobile-overlay');
+
+menuIcon.addEventListener('click', ()=>{
+    menuIcon.classList.toggle('active');
+    menuList.classList.toggle('active');
+
+})
+
+
+//pop up quote
+const requestBtn = document.querySelector('.btn-primary');
+const popUp = document.querySelector('.quote-layer');
+const closeBtn = document.querySelector('.close');
+
+
+requestBtn.addEventListener('click',()=>{
+   popUp.classList.toggle('active')
+})
+closeBtn.addEventListener('click',()=>{
+    popUp.classList.toggle('active')
+})
